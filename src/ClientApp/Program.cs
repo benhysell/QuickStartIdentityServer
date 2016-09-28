77c212a13a8629212f8a25594ddf5e53b3b70ce9
Benjamin Hysell <ben.hysell@gmail.com>
2016-09-28 12:22:32 -0400

    First add

---
+++ src/ClientApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace ClientApp
+{
+    public class Program
+    {
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
+        {
+            var dateStart = DateTime.Now;
+            // discover endpoints from metadata
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            // request token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");                       
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            // call api
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+            for (int i = 0; i < 120000; i++)
+            {
+                var response = await client.GetAsync("http://localhost:5001/identity");
+                if (!response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine(response.StatusCode);
+                    Console.WriteLine($"Start - {dateStart}");
+                    Console.WriteLine($"End - {DateTime.Now}");
+                    Console.WriteLine($"Total - {DateTime.Now - dateStart}");
+                    Console.ReadKey();
+                }
+
+                var content = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(JArray.Parse(content));
+                await Task.Delay(500);
+            }
+            
+            Console.ReadKey();            
+        }
+    }
+}
---
+++ src/QuickStartIdentityServer/config.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Services.InMemory;
+
+namespace QuickStartIdentityServer.BusinessEntities
+{
+    public class Config
+    {
+        public static IEnumerable<Scope> GetScopes()
+        {
+            return new List<Scope>
+            {
+                new Scope
+                {
+                    Name = "api1",
+                    Description = "My API",
+                    ScopeSecrets = new List<Secret>
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = new List<string>
+                    {
+                        "api1"
+                    },         
+                    AccessTokenType = AccessTokenType.Jwt,                              
+                    AccessTokenLifetime = 6,                    
+                },
+                  // resource owner password grant client
+                new Client
+                {
+                    ClientId = "ro.client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = new List<string>
+                    {
+                        "api1"
+                    },
+                    AccessTokenLifetime = 1,
+                }
+            };
+        }
+
+        public static List<InMemoryUser> GetUsers()
+        {
+            return new List<InMemoryUser>
+    {
+        new InMemoryUser
+        {
+            Subject = "1",
+            Username = "alice",
+            Password = "password"
+        },
+        new InMemoryUser
+        {
+            Subject = "2",
+            Username = "bob",
+            Password = "password"
+        }
+    };
+        }
+    }
+}
